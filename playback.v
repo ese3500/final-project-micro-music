22d0c9f7051763a106273bf512e94c25e3e807fd
Nhlanhla Mavuso <141350627+nmavuso@users.noreply.github.com>
2024-04-30 15:41:46 -0400

    Update playback.v

--- playback.v
+++ playback.v
@@ -496,4 +496,43 @@ module uart_finite_state_machine(
           end
 endmodule
 
+(* CORE_GENERATION_INFO = "clock_gen,IP_Integrator,{x_ipVendor=xilinx.com,x_ipLibrary=BlockDiagram,x_ipName=clock_gen,x_ipVersion=1.00.a,x_ipLanguage=VERILOG,numBlks=1,numReposBlks=1,numNonXlnxBlks=0,numHierBlks=0,maxHierDepth=0,numSysgenBlks=0,numHlsBlks=0,numHdlrefBlks=0,numPkgbdBlks=0,bdsource=USER,synth_mode=OOC_per_IP}" *) (* HW_HANDOFF = "clock_gen.hwdef" *) 
+module clock_gen
+   (clk_in1,
+    clk_out1,
+    clk_out2,
+    clk_out3,
+    clk_out4,
+    locked,
+    resetn);
+  (* X_INTERFACE_INFO = "xilinx.com:signal:clock:1.0 CLK.CLK_IN1 CLK" *) (* X_INTERFACE_PARAMETER = "XIL_INTERFACENAME CLK.CLK_IN1, CLK_DOMAIN clock_gen_clk_in1, FREQ_HZ 100000000, FREQ_TOLERANCE_HZ 0, INSERT_VIP 0, PHASE 0.0" *) input clk_in1;
+  (* X_INTERFACE_INFO = "xilinx.com:signal:clock:1.0 CLK.CLK_OUT1 CLK" *) (* X_INTERFACE_PARAMETER = "XIL_INTERFACENAME CLK.CLK_OUT1, CLK_DOMAIN clock_gen_clk_wiz_0_0_clk_out1, FREQ_HZ 100000000, FREQ_TOLERANCE_HZ 0, INSERT_VIP 0, PHASE 0.0" *) output clk_out1;
+  (* X_INTERFACE_INFO = "xilinx.com:signal:clock:1.0 CLK.CLK_OUT2 CLK" *) (* X_INTERFACE_PARAMETER = "XIL_INTERFACENAME CLK.CLK_OUT2, CLK_DOMAIN clock_gen_clk_wiz_0_0_clk_out1, FREQ_HZ 100000000, FREQ_TOLERANCE_HZ 0, INSERT_VIP 0, PHASE 0.0" *) output clk_out2;
+  (* X_INTERFACE_INFO = "xilinx.com:signal:clock:1.0 CLK.CLK_OUT3 CLK" *) (* X_INTERFACE_PARAMETER = "XIL_INTERFACENAME CLK.CLK_OUT3, CLK_DOMAIN clock_gen_clk_wiz_0_0_clk_out1, FREQ_HZ 100000000, FREQ_TOLERANCE_HZ 0, INSERT_VIP 0, PHASE 0.0" *) output clk_out3;
+  (* X_INTERFACE_INFO = "xilinx.com:signal:clock:1.0 CLK.CLK_OUT4 CLK" *) (* X_INTERFACE_PARAMETER = "XIL_INTERFACENAME CLK.CLK_OUT4, CLK_DOMAIN clock_gen_clk_wiz_0_0_clk_out1, FREQ_HZ 100000000, FREQ_TOLERANCE_HZ 0, INSERT_VIP 0, PHASE 0.0" *) output clk_out4;
+  output locked;
+  (* X_INTERFACE_INFO = "xilinx.com:signal:reset:1.0 RST.RESETN RST" *) (* X_INTERFACE_PARAMETER = "XIL_INTERFACENAME RST.RESETN, INSERT_VIP 0, POLARITY ACTIVE_LOW" *) input resetn;
+
+  wire clk_in1_1;
+  wire clk_wiz_0_clk_out1;
+  wire clk_wiz_0_clk_out2;
+  wire clk_wiz_0_clk_out3;
+  wire clk_wiz_0_clk_out4;
+  wire clk_wiz_0_locked;
+
+  assign clk_in1_1 = clk_in1;
+  assign clk_out1 = clk_wiz_0_clk_out1;
+  assign clk_out2 = clk_wiz_0_clk_out2;
+  assign clk_out3 = clk_wiz_0_clk_out3;
+  assign clk_out4 = clk_wiz_0_clk_out4;
+  assign locked = clk_wiz_0_locked;
+  clock_gen_clk_wiz_0_0 clk_wiz_0
+       (.clk_in1(clk_in1_1),
+        .clk_out1(clk_wiz_0_clk_out1),
+        .clk_out2(clk_wiz_0_clk_out2),
+        .clk_out3(clk_wiz_0_clk_out3),
+        .clk_out4(clk_wiz_0_clk_out4),
+        .locked(clk_wiz_0_locked));
+endmodule
+
 
